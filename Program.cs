525d9c69cbba8268f981f91e4366e644c4a26a24
Devran ŞEKER <120737148+devranseker@users.noreply.github.com>
2023-08-10 20:13:01 +0300

    Abstract-Class

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Abstract_Class;
+//furniture = mobilya ,fabric=kumaş,material=malzeme,feature=özelik,sofa=kanepe
+abstract class furniture
+{
+   public string color;
+   public string fabric;
+   public string material;
+    abstract public void feature();
+}
+
+class Table : furniture
+{
+    public override void feature()
+    {
+        Console.WriteLine("Masanın özelikleri ");
+        Console.WriteLine("Renk:" + color);
+        Console.WriteLine("Malzeme:" + material);
+        
+    }
+}
+class Sofa : furniture
+{
+    public override void feature()
+    {
+        Console.WriteLine("Kanepenin özelikleri ");
+        Console.WriteLine("Renk: " + color);
+        Console.WriteLine("Kumaş: " + fabric);
+
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Abstract_Class
+        //Bu sınıftan new anahtar kelimesi kullanılarak bir nesne oluşturulamaz.
+        // Sadece soyut sınıflar içerisinde kullanılabilirler.
+
+        Table table = new Table();
+        table.color = "Siyah";
+        table.material = "Dut ağacı";
+
+        Sofa sofa = new Sofa();
+        sofa.color = "Kahverengi";
+        sofa.fabric = "Keten";
+
+        table.feature();
+        sofa.feature();
+        Console.Read();
+
+    }
+}
+
+
+
+
